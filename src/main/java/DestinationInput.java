e295d68b0dce50779a32ad4a26ecdf64d47f1336
Kristof Majoros <akmajoros1@gmail.com>
2017-11-06 11:17:45 +0100

    DestinationInput, HolidayPLanner and Main classes created

---
+++ src/main/java/DestinationInput.java
@@ -0,0 +1,11 @@
+public class DestinationInput {
+
+  String location;
+
+  String rule;
+
+  public DestinationInput(String location, String rule) {
+    this.location=location;
+    this.rule= rule;
+  }
+}
---
+++ src/main/java/HolidayPlanner.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HolidayPlanner {
+
+  List<String> optimalJourneyPlan = new ArrayList<>();
+
+  public HolidayPlanner() {
+  }
+
+  public List<String> journeyPlanner(List<DestinationInput> listOfDestinations) {
+
+    return optimalJourneyPlan;
+  }
+}
