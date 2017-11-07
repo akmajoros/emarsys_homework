af6a323f8358cfc0f86b7a3a4f115aad12e28c5f
Kristof Majoros <akmajoros1@gmail.com>
2017-11-07 10:14:10 +0100

    shortened variable names

--- src/main/java/HolidayPlanner.java
+++ src/main/java/HolidayPlanner.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class HolidayPlanner {
 
-  List<String> optimalJourneyPlan = new ArrayList<>();
+  List<String> journey = new ArrayList<>();
 
   public HolidayPlanner() {
   }
@@ -14,25 +13,25 @@ public class HolidayPlanner {
     while (listOfDestinations.size() > 0) {
       for (Iterator<DestinationInput> iterator = listOfDestinations.iterator();
           iterator.hasNext(); ) {
-        DestinationInput destinationInput = iterator.next();
+        DestinationInput destination = iterator.next();
 
-        String upperCaseLocation = destinationInput.location.toUpperCase();
-        String upperCaseRule = destinationInput.rule.toUpperCase();
+        String upperCaseLocation = destination.location.toUpperCase();
+        String upperCaseRule = destination.rule.toUpperCase();
 
-        if (destinationInput.location.equals("")) {
+        if (destination.location.equals("")) {
           iterator.remove();
-        } else if(optimalJourneyPlan.contains(upperCaseLocation) && destinationInput.rule.equals("")) {
+        } else if(journey.contains(upperCaseLocation) && destination.rule.equals("")) {
           iterator.remove();
-        } else if (!destinationInput.location.equals("") && destinationInput.rule.equals("")) {
-          optimalJourneyPlan.add(upperCaseLocation);
+        } else if (!destination.location.equals("") && destination.rule.equals("")) {
+          journey.add(upperCaseLocation);
           iterator.remove();
-        } else if (!optimalJourneyPlan.contains(upperCaseLocation) && optimalJourneyPlan.contains(upperCaseRule)) {
-          optimalJourneyPlan.add(upperCaseLocation);
+        } else if (!journey.contains(upperCaseLocation) && journey.contains(upperCaseRule)) {
+          journey.add(upperCaseLocation);
           iterator.remove();
-        } else if (optimalJourneyPlan.contains(upperCaseRule) && optimalJourneyPlan.contains(upperCaseLocation)) {
-          if (optimalJourneyPlan.indexOf(upperCaseRule) > optimalJourneyPlan.indexOf(upperCaseLocation)) {
-            optimalJourneyPlan.remove(upperCaseLocation);
-            optimalJourneyPlan.add(upperCaseLocation);
+        } else if (journey.contains(upperCaseRule) && journey.contains(upperCaseLocation)) {
+          if (journey.indexOf(upperCaseRule) > journey.indexOf(upperCaseLocation)) {
+            journey.remove(upperCaseLocation);
+            journey.add(upperCaseLocation);
             iterator.remove();
           } else {
             iterator.remove();
@@ -40,10 +39,10 @@ public class HolidayPlanner {
         }
       }
     }
-    if (optimalJourneyPlan.size() <= 0){
+    if (journey.size() <= 0){
       throw new Exception("Journey can't be created with no locations given");
     } else {
-      return optimalJourneyPlan;
+      return journey;
     }
   }
 }
